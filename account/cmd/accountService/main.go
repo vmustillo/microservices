a15738178d9baf5d81ca82555d5665e7189505fd
Vincent Mustillo <vmustillo@Vincents-MBP.fios-router.home>
2020-08-01 19:26:46 -0400

    Added postgres docker instance and persisted the data to local storage. Also added a database to the main AccountServer struct and was able to connect and ping the db

--- account/cmd/accountService/main.go
+++ account/cmd/accountService/main.go
@@ -2,21 +2,26 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"flag"
 	"fmt"
+	"log"
 	"net"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
 	gw "github.com/vmustillo/microservices/account/gen"
+	pkg "github.com/vmustillo/microservices/account/pkg"
 )
 
 var (
 	echoEndpoint = flag.String("echo_endpoint", "localhost:9090", "endpoint of account")
 )
 
-type AccountServer struct{}
+type AccountServer struct {
+	Db *sql.DB
+}
 
 func (*AccountServer) GetAccount(ctx context.Context, req *gw.GetAccountRequest) (*gw.Account, error) {
 	return &gw.Account{
@@ -27,13 +32,26 @@ func (*AccountServer) GetAccount(ctx context.Context, req *gw.GetAccountRequest)
 	}, nil
 }
 
-func newAccountServer() *grpc.Server {
+func newAccountServer(actSrv *AccountServer) *grpc.Server {
 
 	srv := grpc.NewServer()
-	gw.RegisterAccountServiceServer(srv, &AccountServer{})
+	gw.RegisterAccountServiceServer(srv, actSrv)
 	reflection.Register(srv)
 
 	return srv
+}
+
+func (s *AccountServer) connectDatabase() error {
+	db, err := pkg.GetConnection("postgres", "postgres", "127.0.0.1", 5433, "account")
+	if err != nil {
+		log.Println("Error connecting to database", err)
+
+		return err
+	}
+
+	s.Db = db
+
+	return nil
 }
 
 func main() {
@@ -43,7 +61,16 @@ func main() {
 		panic(err)
 	}
 
-	accountService := newAccountServer()
+	a := &AccountServer{}
+	a.connectDatabase()
+	err = a.Db.Ping()
+	if err != nil {
+		log.Println("ERROR", err)
+	} else {
+		log.Println("SUCCESS")
+	}
+
+	accountService := newAccountServer(a)
 	fmt.Println("Listening on port 4040")
 	if e := accountService.Serve(listener); e != nil {
 		panic(e)
