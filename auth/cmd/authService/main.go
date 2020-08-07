8c9424e75502997d73ba541e00bff5a338779d01
Vincent Mustillo <vmustillo@Vincents-MBP.fios-router.home>
2020-08-07 19:04:50 -0400

    moved most code out of main into other packes. Main now just connects to the database, initializes kafka producers/consumers, runs the auth consumer in a goroutine, and runs listenAndServe

--- auth/cmd/authService/main.go
+++ auth/cmd/authService/main.go
@@ -1,89 +1,12 @@
 package main
 
 import (
-	"context"
-	"database/sql"
 	"fmt"
 	"log"
 	"net"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
-
-	gen "github.com/vmustillo/microservices/auth/gen"
-	pkg "github.com/vmustillo/microservices/auth/pkg"
+	server "github.com/vmustillo/microservices/auth/internal/server"
 )
-
-type AuthServer struct {
-	Db *sql.DB
-}
-
-func (s *AuthServer) GetUsers(ctx context.Context, req *gen.GetUsersRequest) (*gen.Users, error) {
-	return &gen.Users{
-		Users: []*gen.User{
-			{
-				Id:        "guid1",
-				AccountID: "accountID1",
-				FirstName: "Vin",
-				LastName:  "M",
-			},
-		},
-	}, nil
-}
-
-func (s *AuthServer) CreateUser(ctx context.Context, req *gen.CreateUserRequest) (*gen.CreateUserResponse, error) {
-
-	id, err := pkg.CreateUser(s.Db, req.GetFirstName(), req.GetLastName(), req.GetUsername(), req.GetPassword())
-	if err != nil {
-		log.Println("Error creating user", err)
-
-		return &gen.CreateUserResponse{
-			User: &gen.User{},
-		}, err
-	}
-
-	return &gen.CreateUserResponse{
-		User: &gen.User{
-			Id:        id,
-			FirstName: req.GetFirstName(),
-			LastName:  req.GetLastName(),
-		},
-	}, nil
-}
-
-func (s *AuthServer) Login(ctx context.Context, req *gen.LoginRequest) (*gen.LoginResponse, error) {
-
-	token, err := pkg.Login(s.Db, req.GetUsername(), req.GetPassword())
-	if err != nil {
-		log.Println("Error creating token", err)
-	}
-
-	return &gen.LoginResponse{
-		Token: token,
-	}, nil
-}
-
-func newAuthServer(authSrv *AuthServer) *grpc.Server {
-
-	srv := grpc.NewServer()
-	gen.RegisterAuthServiceServer(srv, authSrv)
-	reflection.Register(srv)
-
-	return srv
-}
-
-func (s *AuthServer) connectDatabase() error {
-	db, err := pkg.GetConnection("postgres", "postgres", "127.0.0.1", 5434, "auth")
-	if err != nil {
-		log.Println("Error connecting to database", err)
-
-		return err
-	}
-
-	s.Db = db
-
-	return nil
-}
 
 func main() {
 
@@ -92,17 +15,24 @@ func main() {
 		panic(err)
 	}
 
-	a := &AuthServer{}
-	a.connectDatabase()
+	a := &server.AuthServer{}
+	a.ConnectDatabase()
+	a.ConnectKafkaAccounts()
+	a.ConnectKafkaAuth()
+
 	err = a.Db.Ping()
+
 	if err != nil {
 		log.Println("ERROR", err)
 	} else {
 		log.Println("SUCCESS")
 	}
 
-	authService := newAuthServer(a)
-	fmt.Println("Listening on port 4040")
+	go a.ConsumeAuth()
+
+	authService := server.NewAuthServer(a)
+
+	fmt.Println("Listening on port 4041")
 	if e := authService.Serve(listener); e != nil {
 		panic(e)
 	}
