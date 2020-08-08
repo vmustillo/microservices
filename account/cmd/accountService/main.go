829e5cf2f256fbc3bfcc3b4f7087067a99dcdca5
Vincent Mustillo <vmustillo@Vincents-MBP.fios-router.home>
2020-08-07 20:52:29 -0400

    Moved most of the main function into the server package. Main now consists of establishing connection to the DB, Kafka, and initializing the server with listenAndServe

--- account/cmd/accountService/main.go
+++ account/cmd/accountService/main.go
@@ -1,58 +1,12 @@
 package main
 
 import (
-	"context"
-	"database/sql"
-	"flag"
 	"fmt"
 	"log"
 	"net"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
-
-	gw "github.com/vmustillo/microservices/account/gen"
-	pkg "github.com/vmustillo/microservices/account/pkg"
+	"github.com/vmustillo/microservices/account/internal/server"
 )
-
-var (
-	echoEndpoint = flag.String("echo_endpoint", "localhost:9090", "endpoint of account")
-)
-
-type AccountServer struct {
-	Db *sql.DB
-}
-
-func (*AccountServer) GetAccount(ctx context.Context, req *gw.GetAccountRequest) (*gw.Account, error) {
-	return &gw.Account{
-		Id:      "guid1",
-		OwnerID: "ownerID1",
-		Balance: 24560.75,
-		Type:    "Checking",
-	}, nil
-}
-
-func newAccountServer(actSrv *AccountServer) *grpc.Server {
-
-	srv := grpc.NewServer()
-	gw.RegisterAccountServiceServer(srv, actSrv)
-	reflection.Register(srv)
-
-	return srv
-}
-
-func (s *AccountServer) connectDatabase() error {
-	db, err := pkg.GetConnection("postgres", "postgres", "127.0.0.1", 5433, "account")
-	if err != nil {
-		log.Println("Error connecting to database", err)
-
-		return err
-	}
-
-	s.Db = db
-
-	return nil
-}
 
 func main() {
 
@@ -61,8 +15,12 @@ func main() {
 		panic(err)
 	}
 
-	a := &AccountServer{}
-	a.connectDatabase()
+	a := &server.AccountServer{}
+	a.ConnectDatabase()
+	a.ConnectKafkaAccounts()
+
+	go a.ConsumeAccounts()
+
 	err = a.Db.Ping()
 	if err != nil {
 		log.Println("ERROR", err)
@@ -70,7 +28,7 @@ func main() {
 		log.Println("SUCCESS")
 	}
 
-	accountService := newAccountServer(a)
+	accountService := server.NewAccountServer(a)
 	fmt.Println("Listening on port 4040")
 	if e := accountService.Serve(listener); e != nil {
 		panic(e)
